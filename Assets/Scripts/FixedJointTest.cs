4d4b3ef270413c4781eb44516403c944fa8b573a
Sam Trapp <set9069@rit.edu>
2014-10-30 00:50:14 -0400

    Messing with joints.

---
+++ Assets/Scripts/FixedJointTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedJointTest : MonoBehaviour {
+	public FixedJoint joint;
+
+	void Update()
+	{
+		//joint.autoConfigureConnectedAnchor = false;
+		joint.anchor = new Vector3(0, 0, 1);
+	}
+}
