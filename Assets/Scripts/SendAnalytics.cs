f07abbf27a2b87e6219fbccfd2f7db5afd2e9b0d
Sam Trapp <set9069@rit.edu>
2014-12-09 23:27:16 -0500

    Prepare for Android deployment and force assets to text.

---
+++ Assets/Scripts/SendAnalytics.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendAnalytics : MonoBehaviour {
+
+
+	
+	void Start () {
+		//AndroidJavaClass jc = new AndroidJavaClass("com.exampleCompany.exampleApp.UnityPlayerNativeActivity");
+		//AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("context");
+	}
+}
