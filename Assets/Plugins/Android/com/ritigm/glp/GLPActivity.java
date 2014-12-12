63a011a743507f3383daf2ef439c37291778ad0c
Sam Trapp <trapp.s.e@gmail.com>
2014-12-11 22:08:33 -0500

    Attempting to integrate Android java files into Unity, and failing.

---
+++ Assets/Plugins/Android/com/ritigm/glp/GLPActivity.java
@@ -0,0 +1,13 @@
+package com.ritigm.glp;
+
+import com.unity3d.player.UnityPlayerActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class GLPActivity extends UnityPlayerActivity
+{
+	protected onCreate(Bundle myBundle)
+	{
+		super.onCreate(myBundle);
+	}
+}
